d00134bfe246765595f3545eb047d0ff5d3e0502
Phattarapon (Arm) <OUTSOURCE1887@setth.onmicrosoft.com>
2024-07-27 00:59:59 +0700

    fix: test commit

---
+++ dist/commitlint.config.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    extends: ['@commitlint/config-conventional'],
+};
+exports.default = config;
+//# sourceMappingURL=commitlint.config.js.map
---
+++ dist/src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./module/app.module");
+const config_1 = require("@nestjs/config");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const configService = app.get(config_1.ConfigService);
+    const port = configService.get('PORT');
+    await app.listen(3000);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
